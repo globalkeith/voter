94b22a00dfca50da4fdd0c2f13fcf242055d6d9f
Keith Salisbury <keith.salisbury@alliants.com>
2015-11-12 22:13:37 +0000

    wip

--- lib/voter.ex
+++ lib/voter.ex
@@ -1,2 +1,19 @@
 defmodule Voter do
+  def start(votes) do
+    votes
+    |> Enum.reduce([0], &accumulate/2)
+    |> tl
+  end
+
+  defp accumulate(x = 1, acc) do
+    acc ++ [List.last(acc) + 1]
+  end
+
+  defp accumulate(x = 2, acc) do
+    acc ++ [List.last(acc) - 1]
+  end
+
+  defp accumulate(_, acc) do
+    acc ++ [List.last(acc)]
+  end
 end
--- test/voter_test.exs
+++ test/voter_test.exs
@@ -2,7 +2,18 @@ defmodule VoterTest do
   use ExUnit.Case
   doctest Voter
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "all 1s" do
+    list = [1,1,1]
+    assert [1,2,3] == Voter.start(list)
+  end
+
+  test "all 0s" do
+    list = [0,0,0]
+    assert [0,0,0] == Voter.start(list)
+  end
+
+  test "all 2s" do
+    list = [2,2,2]
+    assert [-1,-2,-3] == Voter.start(list)
   end
 end
